b3398b6217cce8789b2d7ccf9f236db31858a2f2
NicoleAnd <nicolea@163.com>
2019-04-20 16:38:16 +0800

    update

---
+++ src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1><hr>
+        <h3>{{$store.state.count}}</h3>
+        <div>
+            <button @click="$store.commit('add')">+</button>
+            <button @click="$store.commit('reduce')">-</button>
+        </div>
+    </div>
+</template>
+<script>
+    import store from '@/vuex/store';
+    export default {
+        data(){
+            return {
+                msg:'hello vuex'
+            }
+        },
+        store
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import count from '@/components/Count'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/count',
+            name: 'count',
+            component: count
+        }
+    ]
 })
